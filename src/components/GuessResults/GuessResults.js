74e225a6a1e49b804ec380f694cfa4f682765ea0
Mathieu Longe <mathieu.longe@orange.fr>
2023-11-06 16:12:36 +1100

    Exercise 3 completed

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { range } from "../../utils";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
+import Guess from "../Guess";
 
 function GuessResults({ guesses }) {
   return (
     <div className="guess-results">
       {guesses.map((guess, index) => (
-        <p className="guess" key={index}>
-          {guess}
-        </p>
+        <Guess guess={guess} key={index} />
+      ))}
+      {range(0, NUM_OF_GUESSES_ALLOWED - guesses.length).map((index) => (
+        <Guess key={index} />
       ))}
     </div>
   );
